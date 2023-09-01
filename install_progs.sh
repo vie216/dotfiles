5989b39c60519a2957ae2ef6db798bf89ade6eb2
viedevr <123240767+viedevr@users.noreply.github.com>
2023-09-01 22:35:50 +0300

    Update install_progs.sh

--- install_progs.sh
+++ install_progs.sh
@@ -6,7 +6,8 @@ sudo pacman -Sy
 sudo pacman -S xorg xorg-xinit i3 \
      ly polybar picom dunst rofi \
      brightnessctl bluez bluez-utils wget \
-     alacritty emacs rustup
+     alacritty emacs rustup \
+     zsh zsh-autosuggestions zsh-syntax-highlighting
 
 sudo systemctl enable ly.service bluetooth.service
 sudo systemctl start bluetooth.service
