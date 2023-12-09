d82b6169bfbc6be174b522c8a50d78bc7a222267
viedevr <123240767+viedevr@users.noreply.github.com>
2023-12-09 21:05:34 +0300

    updated init.el

--- home/.config/emacs/init.el
+++ home/.config/emacs/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (custom-set-variables
- '(custom-enabled-themes '(atom-one-dark))
+ '(custom-enabled-themes '(timu-spacegrey))
  '(custom-safe-themes
    '("821c37a78c8ddf7d0e70f0a7ca44d96255da54e613aa82ff861fe5942d3f1efc" "3074fda75f35f990d112fb75681729a74b6c7f15d3e5dfcf80313abb4cd39ed8" "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "0c860c4fe9df8cff6484c54d2ae263f19d935e4ff57019999edbda9c7eda50b8" default))
  '(package-selected-packages
@@ -27,14 +27,16 @@
 (set-frame-parameter nil 'alpha-background 95)
 (add-to-list 'default-frame-alist '(alpha-background . 95))
 
-;; Add convenient bindings
+;; Add some convenient functionality
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings 'meta))
 
-;; Add some functionality
 (setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq completion-auto-help nil)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
@@ -50,13 +52,30 @@
   :bind (("C-." . mc/mark-next-like-this)
          ("C-," . mc/mark-previous-like-this)
          ("C-c ," . mc/mark-all-like-this)
-         :map mc/keymap ("<return>" . nil)))
+         :map
+         mc/keymap ("<return>" . nil)))
 
 (use-package ido-completing-read+
   :config
   (ido-mode t)
   (ido-everywhere t)
-  (ido-ubiquitous-mode t))
+  (setq ido-decorations
+      '("\n-> " ""
+        "\n   "
+        "\n   ..."
+        "[" "]"
+        " [No match]"
+        " [Matched]"
+        " [Not readable]"
+        " [Too big]"
+        " [Confirm]"))
+  (setq ido-enable-flex-matching t)
+  (ido-ubiquitous-mode t)
+  (add-hook 'ido-setup-hook
+            (lambda ()
+              (define-key ido-completion-map (kbd "<down>") 'ido-next-match)
+              (define-key ido-completion-map (kbd "<up>") 'ido-prev-match)
+              (define-key ido-completion-map (kbd "<tab>") 'ido-complete))))
 
 (use-package amx
   :config
